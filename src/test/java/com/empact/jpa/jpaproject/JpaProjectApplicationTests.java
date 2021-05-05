c15a085f119d648301233f7f1f9047e269e84024
Emmanuel Adedeji <emmans4destiny@gmail.com>
2021-05-05 14:24:05 +0300

    JPA project templates

---
+++ src/test/java/com/empact/jpa/jpaproject/JpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empact.jpa.jpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
